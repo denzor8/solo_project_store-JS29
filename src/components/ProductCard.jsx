fe602aaf2e80580314ebc8dd6dab60d8956f48cf
Vasilev Vlad <vladvasiliev894@gmail.com>
2023-03-02 00:23:52 +0600

    update wtf style?

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-
 const ProductCard = ({ item }) => {
   // console.log(item)
   return (
--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.scss";
@@ -6,23 +5,10 @@ import 'macro-css'
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
-=======
-
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.scss";
-import App from "./App";
-import { BrowserRouter } from "react-router-dom";
-
->>>>>>> 3b249985364a7768c05b86f81fff31e9a21e4328
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
     <App />
   </BrowserRouter>
-<<<<<<< HEAD
 );
-=======
-);
->>>>>>> 3b249985364a7768c05b86f81fff31e9a21e4328
